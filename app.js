43449454e714694ef9ec4b6beafa0f6c3ba2f88e
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-26 09:47:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 const express = require('express');
 const app = express();
 const geoip = require('geoip-lite');
-const port = 8080;
-
-/* Normal IP-address*/
-app.get('/ip', function(req, res) {
-  const ip = req.connection.remoteAddress;
-
-  const ipjson = new Object();
-  ipjson.ip = ip;
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify({ ip: ip }));
-  res.end("");
-});
-
-/* GEOIP */const express = require('express');
-const app = express();
-const geoip = require('geoip-lite');
 const dns = require('dns');
 const port = 8080;
 
@@ -40,12 +23,21 @@ app.get('/ip', function(req, res) {
 
 /* GEOIP */
 app.get('/geoip', function(req, res) {
-  const ip = req.connection.remoteAddress;
-  const geo = geoip.lookup(ip);
+  if (req.query.address != "") {
+    const ip = req.query.address;
+    const geo = geoip.lookup(ip);
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify(geo));
-  res.end("");
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(geo));
+    res.end("");
+  } else {
+    const ip = req.connection.remoteAddress;
+    const geo = geoip.lookup(ip);
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(geo));
+    res.end("");
+  }
 });
 
 
@@ -57,25 +49,6 @@ app.get('/host2ip', function(req, res) {
     res.write(JSON.stringify({ address: address, family: family }));
     res.end("");
   });
-});
-
-app.get('/*', function(req, res) {
-  res.writeHead(404, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify({ error: "404"}));
-  res.end("");
-});
-
-app.listen(port, function() {
-  console.log('Express server (http) is listening on *:' + port);
-});
-
-app.get('/geoip', function(req, res) {
-  const ip = req.connection.remoteAddress;
-  const geo = geoip.lookup(ip);
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify(geo));
-  res.end("");
 });
 
 app.get('/*', function(req, res) {
