9ebf5e04be181c5dab806a55921eaf146c3ba610
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-10-09 13:00:32 +0300

    Implemented API for DNS validation

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const express = require('express');
+const dnsPromises = require('dns').promises;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/api/dns/:email', async (req, res) => {
+    let email = req.params.email;
+    let hostname = email.split('@').pop();
+    console.log(hostname);
+    try {
+        res.send(await dnsPromises.resolveMx(hostname));
+    } catch (error) {
+        res.status(404).send({
+            "error": error.code
+        });
+    }
+});
+
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`);
+});
