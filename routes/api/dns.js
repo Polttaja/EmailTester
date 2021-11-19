8b15f135df878acd817b86e4d3c7aeccf4ded5ed
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-11-19 17:44:15 +0200

    comment

--- routes/api/dns.js
+++ routes/api/dns.js
@@ -1,6 +1,7 @@
 const dnsPromises = require('dns').promises;
 
 module.exports = dnsCheck = async (req, res) => {
+    // Check if TLD is in the list before running dns query https://data.iana.org/TLD/tlds-alpha-by-domain.txt
     let email = req.params.email;
     let hostname = email.split('@').pop();
     console.log(hostname);
